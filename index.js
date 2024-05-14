23fbe154afab8e882611bd885ce2725f2af42591
Md Ismail <mdismaeelkhan345@gmail.com>
2024-05-14 23:31:01 +0530

    Updated

--- index.js
+++ index.js
@@ -7,47 +7,54 @@ const port = 4000;
 const dadJokeApi = 'https://api.api-ninjas.com/v1/dadjokes';
 const imageApi = 'https://api.unsplash.com/photos/random';
 
-
-app.get('/api/random/image', async (req, res) => {
+const getRandomImage = async (url) => {
     try {
-        const response = await axios.get(imageApi, {
+        const response = await axios.get(url, {
             headers: {
                 "Authorization": "Client-ID KaAbLeE5ARrqQXhHf2QfqQzt2imDHOiGlKbcBGPu3iY"
             }
         })
-        // console.log(response.data.urls);
+
+        const data = response.data.urls
+        return data;
+
+    } catch (err) {
+        console.error('Error fetching random image:', err);
+        throw err;
+    }
+}
+
+const getRandomJoke = async (url) => {
+    try {
+        const response = await axios.get(url, {
+            headers: {
+                "X-Api-Key": 'k8UOMDTE6d4+mpq0dZ1Yyw==mpuyhr79O29vxuOW'
+            }
+        })
+        const data = response.data;
+        return data;
+    } catch (err) {
+        console.error('Error fetching random joke:', err);
+        throw err;
+    }
+    // console.log(data);
+}
+
+app.get('/api/random/image/joke', async (req, res) => {
+    try {
+        const jokeData = await getRandomJoke(dadJokeApi)
+        const imageData = await getRandomImage(imageApi)
         res.json({
             sucess: true,
-            message: 'RandomImage generated SuccessFully',
-            image: response.data.urls,
+            message: 'Random image and jake generated SuccessFully',
+            joke: jokeData,
+            image: imageData
         })
     } catch (err) {
 
         res.status(500).json({
             sucess: false,
             message: 'Internal Server Error'
-        })
-    }
-});
-
-app.get('/api/random/joke', async (req, res) => {
-
-    try {
-        const response = await axios.get(dadJokeApi, {
-            headers: {
-                "X-Api-Key": 'k8UOMDTE6d4+mpq0dZ1Yyw==mpuyhr79O29vxuOW'
-            }
-        })
-        res.json({
-            sucess: true,
-            message: 'RandomJoke generated SuccessFully',
-            joke: response.data,
-        })
-
-    } catch (err) {
-        res.status(500).json({
-            sucess: false,
-            message: 'Internal Server Error',
         })
     }
 });
